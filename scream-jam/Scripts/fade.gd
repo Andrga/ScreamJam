305af0d21658ee17a43347d6d8dfa41e7cdf55ea
Cynthia Tristán <cyntrist@ucm.es>
2024-10-18 02:23:01 +0200

    fade? perdon estoy metiendo mucha morralla

---
+++ scream-jam/Scripts/fade.gd
@@ -0,0 +1,27 @@
+extends CanvasLayer
+
+signal transitioned
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func transition():
+	$AnimationPlayer.play("fade_in");
+	
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "fade_in":
+		print("DENTRO FADE")
+		emit_signal("transitioned")
+		$AnimationPlayer.play("fade_out")
+	elif anim_name == "fade_out":
+		print("FUERA FADE")
+		
