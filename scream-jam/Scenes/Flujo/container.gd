89f35b60035e134a1ccb948c6e53a58ef7334e0d
Cynthia Tristán <cyntrist@ucm.es>
2024-10-20 16:22:48 +0200

    ughhhh por la puta cara

--- scream-jam/Scenes/Flujo/container.gd
+++ scream-jam/Scenes/Flujo/container.gd
@@ -1,10 +1,9 @@
-extends Area2D
+extends Container
 
-@export var to_scene : PackedScene = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	self.input_pickable = true
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,5 +12,4 @@ func _process(delta: float) -> void:
 
 
 func _on_mouse_entered() -> void:
-	print("JODEEEEEEEEEEEEEEEEEEEEEER")
-	
+	print("JODER")
