78575cd6e6d183e57197cc204881b2532df40679
Cynthia Tristán <cyntrist@ucm.es>
2024-10-22 23:12:28 +0200

    por qué no se me había ocurrido antes

--- scream-jam/Prefabs/basura.gd
+++ scream-jam/Prefabs/basura.gd
@@ -3,11 +3,11 @@ extends Node2D
 var texture_normal = load("res://Images/cenicero.png")
 var texture_hover = load("res://Images/cenicero_hover.png")
 var mousepos: Vector2
+var buton : TextureButton = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	buton = $TextureButton
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -15,16 +15,14 @@ func _process(delta: float) -> void:
 
 
 func _on_area_2d_mouse_entered() -> void:
-	var buton = $TextureButton
 	buton.texture_normal = texture_hover
 
 func _on_area_2d_mouse_exited() -> void:
-	var buton = $TextureButton	
 	buton.texture_normal = texture_normal
 
 
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	print("JODEEEER")
-
 func _on_area_2d_area_entered(area: Area2D) -> void:
-	print("JODEEEER")
+	buton.texture_normal = texture_hover
+
+func _on_area_2d_area_exited(area: Area2D) -> void:
+	buton.texture_normal = texture_normal
