45fb66ba0c70f42836038f2ead996eed967b6a97
Cynthia Tristán <cyntrist@ucm.es>
2024-10-23 20:22:26 +0200

    main

--- scream-jam/Scripts/Flujo/main_menu.gd
+++ scream-jam/Scripts/Flujo/main_menu.gd
@@ -1,22 +1,21 @@
 class_name MainMenu
 extends Control
 
-@onready var start_button: Button = $MarginContainer/VBoxContainer/Start as Button
-@onready var exit_button: Button = $MarginContainer/VBoxContainer/Exit as Button
-@onready var v_box_container: VBoxContainer = $MarginContainer/VBoxContainer/VBoxContainer
+@onready var start_button: Button = $Start as Button
+@onready var exit_button: Button = $VBoxContainer/Exit as Button
+@onready var v_box_container: VBoxContainer = $VBoxContainer/VBoxContainer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	start_button.button_down.connect(_on_start_down)
-	exit_button.button_down.connect(_on_exit_down)
+	pass
+	#start_button.button_down.connect(_on_start_down)
+	#exit_button.button_down.connect(_on_exit_down)
 
 func _on_start_down() -> void:
-	Global.current_scene = Global.Scenes.MAIN_MENU
-	Global.to_scene = Global.Scenes.CLAVIJAS
-	Global.totransition.emit()
+	pass
 	
 func _on_exit_down() -> void:
-	get_tree().quit()
+	pass
 
 func _show_lenguages()-> void:
 	v_box_container.visible = not v_box_container.visible 
@@ -28,3 +27,16 @@ func _set_ingles()->void:
 func _set_espaniol()->void:
 	JsonData._load_lenguage("res://Jsons/spanish.json")
 	_show_lenguages()
+
+
+func _on_start_button_down() -> void:
+	Global.current_scene = Global.Scenes.MAIN_MENU
+	Global.to_scene = Global.Scenes.CLAVIJAS
+	Global.totransition.emit()
+
+
+
+
+func _on_exit_button_down() -> void:
+	get_tree().quit()
+	pass # Replace with function body.
