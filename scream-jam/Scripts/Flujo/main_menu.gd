4e2e15c10489df734e0a9c562f477649d6ec1bb4
Andrés García Navarro <andrga37@ucm.es>
2024-10-23 20:26:39 +0200

    amai

--- scream-jam/Scripts/Flujo/main_menu.gd
+++ scream-jam/Scripts/Flujo/main_menu.gd
@@ -12,7 +12,9 @@ func _ready() -> void:
 	#exit_button.button_down.connect(_on_exit_down)
 
 func _on_start_down() -> void:
-	pass
+	Global.current_scene = Global.Scenes.MAIN_MENU
+	Global.to_scene = Global.Scenes.CONTEXT
+	Global.totransition.emit()
 	
 func _on_exit_down() -> void:
 	pass
@@ -21,11 +23,13 @@ func _show_lenguages()-> void:
 	v_box_container.visible = not v_box_container.visible 
 
 func _set_ingles()->void:
-	JsonData._load_lenguage("res://Jsons/spanish.json")
+	JsonData._load_lenguage("res://Jsons/englis.json")
+	Global.lenguaje = 0
 	_show_lenguages()
 
 func _set_espaniol()->void:
 	JsonData._load_lenguage("res://Jsons/spanish.json")
+	Global.lenguaje = 1
 	_show_lenguages()
 
 
